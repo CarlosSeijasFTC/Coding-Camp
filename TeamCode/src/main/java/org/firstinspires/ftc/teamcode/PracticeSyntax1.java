e8166ab4dd27b2cb05f10064cb948cde3a4b44d1
elsej <cseijas28@cathedral-elpaso.org>
2026-06-15 13:31:53 -0600

    06/15/2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PracticeSyntax1.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+
+//This is the File/Class that was supposed to be created on Day 1's practice slide.
+public class PracticeSyntax1 {
+    private int stage = 0;
+    protected double motorPosition = -0.3;
+    public String stageName = "First Stage";
+    final double MOTOR_CONSTANT = 0.2756;
+
+    /*remember to practice the order in which the things are put to create small classes
+         1.attribute (Mos of the times public)
+         2.type of class
+         3.name to address
+         4.equal sign and value
+         5.semicolon
+     */
+
+    //Things in gray are comments, btw. They do not affect the code.
+}
